bd3c9fd94a6141ed7f8e113c845c3a57f5686142
Pawan Kumar Siwakoti <pksiwako@deakin.edu.au>
2022-09-07 19:34:43 +1000

    made the bookingScreen better

--- src/screens/BookingScreens.js
+++ src/screens/BookingScreens.js
@@ -26,25 +26,9 @@ export default function BookingFormScreen() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs
-      .sendForm(
-        'service_nmk2gpb',
-        'template_mu49wdx',
-        form.current,
-        'rTdvbEOK39QmS1FK8'
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-    setFormErrors(validate(state));
-    setIsSubmit(true);
+    setFormErrors(validate(state)) 
+    setIsSubmit(true);     
   };
-
   const validate = (values) => {
     const errors = {};
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
@@ -58,32 +42,51 @@ export default function BookingFormScreen() {
     } else if (values.MobileNumber.length < 10) {
       errors.MobileNumber = 'MobileNumber must be 10 characters';
     }
-
     if (!values.email) {
-      errors.email = 'Emails is required';
+      errors.email = 'Email is required';
     } else if (!regex.test(values.email)) {
       errors.email = 'This is not a valid email format!';
     }
     if (!values.cleaning) {
-      errors.cleaning = 'Please select the type of cleaning. It is required';
+      errors.cleaning = 'Please select the type of cleaning required';
     }
   
     if (!values.description) {
-      errors.description = 'Please select one Option';
+      errors.description = 'Please describe about the job.';
     }
     return errors;
   };
+
+  function sendEmail(){
+      emailjs
+      .sendForm(
+        'service_nmk2gpb',
+        'template_mu49wdx',
+        form.current,
+        'rTdvbEOK39QmS1FK8'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      )   
+  }
+
   return (
     <div>
-      {Object.keys(formErrors).length === 0 && isSubmit ? (
+      {Object.keys(formErrors).length === 0  && isSubmit ? (  
         <div>
+          {sendEmail()}
           <MessageBox variant="success">
             <p>
-              Your email has been sent with following details to our technician,{' '}
-              <br /> our technician will get back to you soon.
+              Your email has been sent,{' '}
+              <br /> our team member will get back to you soon.
             </p>
           </MessageBox>
-
+            
           <h2>Your details</h2>
           <h6>Name: {state.FullName}</h6>
           <h6>Mobile number: {state.MobileNumber}</h6>
@@ -91,18 +94,20 @@ export default function BookingFormScreen() {
           <h6>ComputerType: {state.cleaning}</h6>
           <h6>Description: {state.description}</h6>
 
-          <Link className="nav-link" to="/payment">
+          <Link className="nav-link" to="/home">
             <Button className="packages" variant="success">
-              Add to cart
+              Return to Home
             </Button>
           </Link>
+          
         </div>
       ) : (
         <div>
-          <h5>Please fill the Form to book.</h5>
+          <h5 style={{textAlign: "center"}}>Please fill the Form to book.</h5>
         </div>
       )}
       <form
+      style={{marginLeft:"6rem"}}
         hidden={Object.keys(formErrors).length === 0 && isSubmit}
         ref={form}
         onSubmit={handleSubmit}
@@ -161,7 +166,7 @@ export default function BookingFormScreen() {
             <option value="HouseKeeping">HouseKeeping</option>
             <option value="Others">Others</option>
           </select>
-          <p style={{ color: 'red' }}>{formErrors.computer}</p>
+          <p style={{ color: 'red' }}>{formErrors.cleaning}</p>
         </label>
         <br />
 
@@ -179,13 +184,6 @@ export default function BookingFormScreen() {
         </label>
         <br />
         <Button
-          disabled={
-            !state.FullName ||
-            !state.MobileNumber ||
-            !state.email ||
-            !state.cleaning ||
-            !state.description
-          }
           type="submit"
           value="Send"
           className="formButton"
@@ -195,4 +193,4 @@ export default function BookingFormScreen() {
       </form>
     </div>
   );
-}
+      }
